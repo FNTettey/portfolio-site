9c2df2128885b4686cf30c9dfb147484a626aa53
Francis Tettey <ftettey@sandtech.com>
2026-05-05 08:39:03 +0100

    Adding Interview Atlas to portfolio

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -12,11 +12,22 @@ export default function Projects() {
   }, []);
 
   return (
-    <Container maxWidth="lg" sx={{ py: 6, bgcolor: "background.default" }}>
-      <Typography variant="h4" color="primary.main" pb="10px" gutterBottom>All Projects</Typography>
-      <Grid2 container spacing={4}>
+    <Container
+      maxWidth={false}
+      sx={{
+        py: 6,
+        bgcolor: "background.default",
+        width: "100%",
+        mx: "auto",
+      }}
+    >
+      <Typography variant="h4" color="primary.main" pb="10px" gutterBottom>
+        All Projects
+      </Typography>
+
+      <Grid2 container spacing={3}>
         {projects.map((p) => (
-          <Grid2 item xs={12} sm={6} md={4} key={p.slug}>
+          <Grid2 size={{ xs: 12, sm: 6, md: 4 }} key={p.slug}>
             <ProjectCard project={p} />
           </Grid2>
         ))}
